97a5b4ae19e9540cf6dbf8e44fa2d50d6a23c30e
TrungLee <tuantrungbeo@gmail.com>
2026-03-21 00:33:07 +0700

    Them cai file nx di

---
+++ src/Hero.java
@@ -0,0 +1,13 @@
+public class Hero {
+    private long health;
+    private String name;
+
+    public Hero(String name, long health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public void getHit() {
+        System.out.println(this.name + " Huhhhhh " + (this.health - 10));
+    }
+}
